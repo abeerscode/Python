fccd7e72dd7929b21ab8dc508c3945e934bba4a5
“abeerscode” <md.abeerhasan71@gmail.com>
2025-10-14 12:36:31 +0600

    User Input & Output

---
+++ ptc1.py
@@ -0,0 +1,7 @@
+# User Input and display Output
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello, " + name + "! You are " + age + " years old.")
+print(f"Hello, {name}! You are {age} years old.")
+print("Hello, {}! You are {} years old.".format(name, age))
